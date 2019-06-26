3a96c58db7a6c2c5156b18ff5a91ad8457a934a7
David Goligorsky <d.goligorsky@gmail.com>
2019-06-25 20:50:02 -0400

    set up styles and starting to add interactivity with keypress events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+)
+
+class App extends Component {
+
+  // with create-react-app we can write out methods
+  // as arrow functions, meaning we don't need the constructor and bind
+  handleChange = event => {
+
+    const {value} = event.target
+    if (value.length > 2) {
+
+    }
+  }
+
+  handleKeyPress = event => {
+    const {value} = event.target
+
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`search for ${value}`)
+    }
+  }
+
+  // when we have two or more characters in our search box
+  // and we have also pressed enter, we then want to run a search
+
+  render() {
+    return (
+      <div className="page">
+          <Header />
+          <div className="search grid">
+            {/* our stack of gif images */}
+            <input className="input grid-item" placeholder="Type something" 
+            onChange={this.handleChange} 
+            onKeyPress={this.handleKeyPress}
+            />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
